11fd82306b6762c4f85ca122d3ba13fc830b78c2
Paul Panserrieu <ppa@zenexity.com>
2011-02-27 11:29:27 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+/**
+ * app.js
+ */
+
+function onReady() {
+
+    function $(id) { return document.getElementById(id);}
+
+    if (Crop.isPossible()) {
+        Crop.init({ 
+            cls: "image-cropper",
+            height:400,
+            width:600,
+            debug:true 
+        });
+
+        $("crop-button").addEventListener("click", function (evt) {
+                evt.preventDefault();
+                evt.stopPropagation();
+                Crop.crop();
+                console.log(Crop.getImage());
+        }, false);
+
+        $("crop-button").addEventListener("selectstart", function (evt) {
+                evt.preventDefault();
+                evt.stopPropagation();
+        }, false);
+    }
+    else {
+        alert("No Crop!!!");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", onReady, false);
